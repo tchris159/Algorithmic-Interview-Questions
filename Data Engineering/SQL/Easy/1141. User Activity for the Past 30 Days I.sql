18cb2583e5dc4f5ed8b4a9384c70a581af97b1c7
Christopher Truong <31805137+tchris159@users.noreply.github.com>
2022-04-03 00:14:55 -0700

    Update 1141. User Activity for the Past 30 Days I.sql

--- Data Engineering/SQL/Easy/1141. User Activity for the Past 30 Days I.sql
+++ Data Engineering/SQL/Easy/1141. User Activity for the Past 30 Days I.sql
@@ -1,1 +1,20 @@
 
+/*
+
+Write an SQL query to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
+
+Return the result table in any order.
+
+*/
+
+
+SELECT 
+    activity_date AS day, COUNT(DISTINCT(user_id)) AS active_users
+
+FROM 
+    activity
+
+WHERE activity_date between subdate('2019-07-27', 29) AND '2019-07-27'
+
+GROUP BY 
+    activity_date
